4bc9b1696b2367b12c593510d7ca498eca70be7d
t-miya2023 <146524763+t-miya2023@users.noreply.github.com>
2023-10-23 11:07:55 +0900

    Add files via upload

---
+++ kadai_tutorial_output/samurai_university/category.php
@@ -0,0 +1,122 @@
+
+<?php get_header(); ?>
+
+    <!-- Home -->
+
+    <div class="home">
+      <div class="breadcrumbs_container">
+        <div class="image_header">
+          <div class="header_info">
+            <?php  
+              $cat = get_the_category();
+              
+              $catslug = $cat[0]->slug;
+              $catname = $cat[0]->cat_name;
+            ?>
+            <div><?php echo $catslug; ?></div>
+            <div><?php echo $catname; ?></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Course -->
+
+    <div class="course">
+      <div class="row content-body">
+        <!-- Course -->
+        <div class="col-lg-8">
+          <!-- Course Tabs -->
+          <div class="course_tabs_container">
+            <div class="tab_panels">
+              <!-- Description -->
+              <div class="tab_panel">
+                <div class="tab_panel_title"><?php echo $catname; ?></div>
+                <div class="tab_panel_content">
+                  <div class="tab_panel_text">
+                    <!-- news loop from here-->
+                    <?php  
+                      if(have_posts()):
+                        while(have_posts()):the_post();
+                    ?>
+                    <div class="news_posts_small">
+                      <div class="row">
+                        <div class="col-lg-2 col-md-2 col-sx-12">
+                          <div class="calendar_news_border">
+                            <div class="calendar_news_border_1">
+                              <div class="calendar_month">
+                                <?php 
+                                  if(is_category('event')){
+                                    echo post_custom('month');
+                                  }else{
+                                    echo get_post_time('F');
+                                  }
+                                ?>
+                              </div>
+                              <div class="calendar_day">
+                                <span>
+                                  <?php 
+                                    if(is_category('event')){
+                                      echo post_custom('day');
+                                    }else{
+                                      echo get_post_time('d');
+                                    } ?>
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <?php
+                            if(is_category('graduates')){
+                              echo '<div class="calendar_hour">
+                          </div>';
+                            } ?>
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-sx-12">
+                          <div class="news_post_small_title">
+                            <a href="<?php the_permalink(); ?>">
+                              <?php the_title(); ?>
+                            </a>
+                          </div>
+                          <div class="news_post_meta">
+                            <ul>
+                              <li>
+                                <?php  
+                                  if(is_category('graduates')){
+                                    $content = wp_trim_words(get_the_content(),100,'');
+                                    $replace = preg_replace('/コメント.+$/u','',$content);
+                                    echo $replace;
+                                    //echo $content;
+                                  }else{
+                                $content = wp_trim_words(get_the_content(),100,'...');
+                                echo $content;
+                                  }?>
+                              </li>
+                            </ul>
+                          </div>
+                          <div class="read_continue">
+                            <button onclick="location.href='<?php the_permalink(); ?>'">詳細を見る</button>
+                          </div>
+                        </div>
+                      </div>
+                      <hr />
+                    </div>
+                    <!-- news loop until here-->
+                        <?php  
+                          endwhile;
+                        endif;
+                        ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Course Sidebar -->
+        <div class="col-lg-4" style="background-color: #2b7b8e33">
+          <?php get_sidebar(); ?>
+        </div>
+      </div>
+    </div>
+
+<?php get_footer(); ?>
